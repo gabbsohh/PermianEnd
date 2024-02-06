ce9fc25ea3864125772592f0faec6b31123fd386
gabbsohh <ortegagabriela84@yahoo.com>
2024-02-06 13:15:14 -0600

    Pause Menu Added
    Pause menu added to all three levels with resume, settings, and main menu buttons.

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject pauseMenuCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Play();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+    }
+
+    void Stop()
+    {
+        pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0.0f;
+        paused = true;
+    }
+
+    public void Play()
+    {
+        pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1.0f;
+        paused = false;
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
