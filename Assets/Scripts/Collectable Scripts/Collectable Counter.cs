58284e1af4a1d3f67d17bc3cdf90c28c2fb06767
gabbsohh <ortegagabriela84@yahoo.com>
2024-03-03 13:23:12 -0600

    Collectables Mechanic

---
+++ Assets/Scripts/Collectable Scripts/Collectable Counter.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectableCounter : MonoBehaviour
+{
+    public static CollectableCounter instance;
+
+    public TMP_Text collectableText;
+    public int currentCollectable = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collectableText.text = "COLLECTABLES: " + currentCollectable.ToString();   
+    }
+
+    // Update is called once per frame
+    public void IncreaseCollectables(int v)
+    {
+        currentCollectable += v;
+        collectableText.text = "COLLECTABLES: " + currentCollectable.ToString() ;
+    }
+}
