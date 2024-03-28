a7c946f23a59f98ce0d5fff459d8242c79e5cf85
gabbsohh <ortegagabriela84@yahoo.com>
2024-03-28 02:27:54 -0500

    messing with life counter
    committed with buggy life counter. still working on it

--- Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -9,8 +9,11 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int maxHealth = 3;
     [SerializeField] int currentHealth;
+    [SerializeField] int maxLives = 3;
+    [SerializeField] int currentLives;
 
-    [SerializeField] private Image healthBarForeground; 
+    [SerializeField] private Image healthBarForeground;
+    [SerializeField] private TMP_Text livesText;
 
     public PlayerMovement playerMovement;
     public UIManager uiManager;
@@ -26,6 +29,9 @@ public class PlayerHealth : MonoBehaviour
     void Awake()
     {
         currentHealth = maxHealth;
+        currentLives = PlayerPrefs.GetInt("CurrentLives", maxLives);
+        UpdateHealthBar();
+        UpdateLivesUI();
     }
 
     // Update is called once per frame
@@ -37,13 +43,19 @@ public class PlayerHealth : MonoBehaviour
     public void GetHurt(int damage)
     {
         currentHealth -= damage;
-        healthBarForeground.fillAmount = currentHealth / (float) maxHealth;
+        UpdateHealthBar();
+
         Debug.Log("Player took damage.");
-        if(currentHealth <= 0 && !isDead)
+
+        if(currentHealth <= 0 && currentLives > 0 && !isDead)
         {
+            currentLives--;
             isDead = true;
             StartCoroutine(Die());
             uiManager.GameOver();
+            UpdateLivesUI();
+            PlayerPrefs.SetInt("CurrentLives", currentLives);
+            
         }
     }
 
@@ -54,15 +66,39 @@ public class PlayerHealth : MonoBehaviour
         // All movement stops, and the player is destroyed afterwards.
         gameObject.GetComponent<PlayerMovement>().StopMovement();
         yield return new WaitForSeconds(1);
-        Destroy(gameObject);
+        if (currentLives > 0)
+        {
+            currentHealth = maxHealth;
+            isDead = false;
+            UpdateHealthBar();
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void Heal(int healAmount)
     { 
         currentHealth += healAmount;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
+    }
 
+    void UpdateHealthBar()
+    {
         healthBarForeground.fillAmount = currentHealth / (float) maxHealth;
+    }
+
+    void UpdateLivesUI()
+    {
+        livesText.text = "LIVES: " + currentLives;
+    }
+
+    public void CollectHealthCollectable(int healAmount)
+    {
+        Debug.Log("Health increased by 1");
+        Heal(healAmount);
     }
 
 }
