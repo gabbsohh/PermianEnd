552ed8f4ac74d54534e6265de31ed541687dbbcc
gabbsohh <ortegagabriela84@yahoo.com>
2024-03-28 10:44:44 -0500

    falling platform added
    may need adjustments when tilemaps are added to project

---
+++ Assets/Scripts/FallingPlatformScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatformScript : MonoBehaviour
+{
+    private Vector2 defaultPos;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] float fallDelay;
+    [SerializeField] float respawnTime;
+
+    private void Start()
+    {
+        defaultPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(PlatformDrop());
+        }
+    }
+
+    IEnumerator PlatformDrop()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        yield return new WaitForSeconds(respawnTime);
+        Reset();
+    }
+
+    private void Reset()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        transform.position = defaultPos;
+    }
+}
