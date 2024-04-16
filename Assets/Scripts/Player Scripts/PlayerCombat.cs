9e18154e336a1d7496506242e6debd33d5cab2fd
gabbsohh <ortegagabriela84@yahoo.com>
2024-04-16 10:52:58 -0500

    Revert "Merge branch 'main' into HealthUI"
    This reverts commit 3c992b64d6cd65094c8f495fc52b154d7c07585a, reversing
    changes made to fa73d37ecf17e92a5f3f746a147576eb9a91341d.

--- Assets/Scripts/Player Scripts/PlayerCombat.cs
+++ Assets/Scripts/Player Scripts/PlayerCombat.cs
@@ -8,7 +8,6 @@ public class PlayerCombat : MonoBehaviour
 
     public float attackRate = 0.5f;
     public float canAttack = -1f;
-    public float canSwitch = -1f;
 
     public Transform attackPoint;
     public float attackArea = 0.75f;
@@ -16,68 +15,19 @@ public class PlayerCombat : MonoBehaviour
 
     public int attackDamage = 1;
 
-    public List<string> weapons;
-    string currentWeapon = "";
-    int weaponIndex = 0;
-
-    public bool usingPick;
-
     // Start is called before the first frame update
     void Start()
     {
-        // Adds the default weapon to the weapon list.
-        weapons.Add("Blade");
-        currentWeapon = weapons[0];
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Checks to see if button is being pressed to perform certain actions, with a cooldown to prevent spam.
         if(Input.GetKeyDown(KeyCode.Q) && Time.time > canAttack)
         {
             Attack();
         }
-
-        if(Input.GetKeyDown(KeyCode.E) && Time.time > canSwitch)
-        {
-            SwitchWeapon();
-        }
-
-        // Checks the player's current weapon and adjusts stats accordingly.
-        if(currentWeapon == "Blade")
-        {
-            attackDamage = 1;
-            attackRate = 0.35f;
-            usingPick = false;
-            // Use animation set for blade.
-        }
-
-        if(currentWeapon == "Pickaxe")
-        {
-            attackDamage = 2;
-            attackRate = 1.0f;
-            usingPick = true;
-            // Use animation set for pickaxe.
-        }
-    }
-
-    void SwitchWeapon()
-    {
-        // Checks to see if the amount of weapons in the list is more than 1 to warrant switching.
-        if(weapons.Count > 1)
-        {
-            // Weapon Index Increases whenever weapon switch is called, going back to the first position if it happens to reach the end.
-            weaponIndex++;
-            if(weaponIndex >= weapons.Count)
-            {
-                weaponIndex = 0;
-            }
-            currentWeapon = weapons[weaponIndex];
-            Debug.Log("Weapon switched to " + currentWeapon + ".");
-        }
-        else
-        Debug.Log("Not enough weapons to switch between!");
     }
 
     void Attack()
@@ -94,6 +44,7 @@ public class PlayerCombat : MonoBehaviour
             Debug.Log(enemy.name + " hit.");
             enemy.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
         }
+
 
         canAttack = Time.time + attackRate;
     }
