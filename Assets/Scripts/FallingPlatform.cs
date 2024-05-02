2b2624a5796d2218edc7c88685956af07a3ecc5a
gabbsohh <ortegagabriela84@yahoo.com>
2024-05-02 13:31:19 -0500

    FallingPlatform Update

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     private Vector2 defaultPos;
+    private bool isFalling;
+
+    private Renderer platformRenderer;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] float fallDelay;
@@ -14,12 +17,14 @@ public class FallingPlatform : MonoBehaviour
     {
         defaultPos = transform.position;
         rb = GetComponent<Rigidbody2D>();
+        platformRenderer = GetComponent<Renderer>();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Player")
+        if (!isFalling && other.gameObject.tag == "Player")
         {
+            isFalling = true;
             StartCoroutine(PlatformDrop());
         }
     }
@@ -28,13 +33,17 @@ public class FallingPlatform : MonoBehaviour
     {
         yield return new WaitForSeconds(fallDelay);
         rb.bodyType = RigidbodyType2D.Dynamic;
+        platformRenderer.enabled = false;
         yield return new WaitForSeconds(respawnTime);
         Reset();
     }
 
     private void Reset()
     {
+        platformRenderer.enabled = true;
+
         rb.bodyType = RigidbodyType2D.Static;
         transform.position = defaultPos;
+        isFalling = false;
     }
 }
