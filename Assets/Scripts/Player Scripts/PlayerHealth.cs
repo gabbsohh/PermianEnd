89d37eb7ee02f22783e50676897a5dd2f7d7bcc5
gabbsohh <ortegagabriela84@yahoo.com>
2024-05-08 12:34:37 -0500

    Update PlayerHealth.cs

--- Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -8,74 +8,84 @@ using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] int maxHealth;
+    [SerializeField] public int maxHealth = 3;
     [SerializeField] public int currentHealth;
     [SerializeField] private Rigidbody2D rb;
 
+    [SerializeField] private AudioClip hurtSoundClip;
+
+    [SerializeField] public Vector3 respawnPoint;
 
     public bool isDead;
 
     public PlayerMovement playerMovement;
-    public HealthUI healthUI;
     public UIManager uiManager;
 
-    // respawn?
-    public Transform respawnPosition;
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numFlashes;
+    private SpriteRenderer spriteRend;
+    [SerializeField] private ParticleSystem deathParticle;
+
+    public GameObject StompChecker;
 
     private void Start()
     {
-        currentHealth = maxHealth;
-        GameData.health = maxHealth;
-        healthUI.SetMaxHearts(maxHealth);
+        maxHealth = currentHealth;
         isDead = false;
+        spriteRend = GetComponent<SpriteRenderer>();
+        StompChecker = transform.GetChild(2).gameObject;
     }
 
     private void Update()
     {
-        
+
     }
 
     public void GetHurt(int damage)
     {
         currentHealth -= damage;
-        healthUI.UpdateHearts(currentHealth);
-        GameData.health = currentHealth;
         Debug.Log("Player took damage.");
+        AudioManager.instance.PlaySoundFXClip(hurtSoundClip, transform, 0.5f);
         if (currentHealth <= 0)
         {
-            Die();
+            Debug.Log("Player's lives went down by 1");
+            StartCoroutine(Die());
+            isDead = true;
         }
-
+        else
+        {
+            StartCoroutine(Invulnerability());
+        }
     }
 
     public IEnumerator Die()
     {
         Debug.Log("Player has died.");
-        // death anim
-
-        transform.position = respawnPosition.position;
+        // Death Animation for Player goes here.
+        deathParticle.Play();
 
         // All movement stops, all collision is removed and the player is destroyed afterwards.
+        // Uses the player's movement script reduce their speed and jump so they can't move.
         gameObject.GetComponent<PlayerMovement>().StopMovement();
-
-        // Rigidbody stays in place to prevent player from falling off the map mario-style.
-        rb.constraints = RigidbodyConstraints2D.FreezeAll;
-
-        // Box Collider turns into a trigger to prevent collision with enemies during death.
-        gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+        yield return new WaitForSeconds(1);
 
         // Change the number here to the duration of the death animation so the whole thing plays out.
         yield return new WaitForSeconds(3);
-        Destroy(gameObject);
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        Respawn();
+        yield return new WaitForSeconds(3);
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        isDead = false;
+        gameObject.GetComponent<PlayerMovement>().ResumeMovement();
     }
 
     public void Heal(int healAmount)
-    { 
+    {
         currentHealth += healAmount;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-        GameData.health = currentHealth;
-        healthUI.UpdateHearts(currentHealth);
+
     }
+
 
     public void CollectHealthCollectable(int healAmount)
     {
@@ -83,4 +93,24 @@ public class PlayerHealth : MonoBehaviour
         Heal(healAmount);
     }
 
+    private IEnumerator Invulnerability()
+    {
+        // Following script causes Player to ignore enemy collision after getting hurt, and disables the jump stun temporarily.
+        Physics2D.IgnoreLayerCollision(9, 6, true);
+        StompChecker.GetComponent<BoxCollider2D>().enabled = false;
+        for (int i = 0; i < numFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(1);
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(1);
+        }
+        StompChecker.GetComponent<BoxCollider2D>().enabled = true;
+        Physics2D.IgnoreLayerCollision(9, 6, false);
+    }
+
+    public void Respawn()
+    {
+        transform.position = respawnPoint;
+    }
 }
