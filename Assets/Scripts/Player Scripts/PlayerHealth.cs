586e6a07113f014b2e27600d51ce65c391ea09f5
gabbsohh <ortegagabriela84@yahoo.com>
2024-05-08 12:42:06 -0500

    Player Health script update
    Respawn and Invulnerability added to script

--- Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -13,15 +13,19 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private ParticleSystem deathParticle;
 
+    [SerializeField] private AudioClip hurtSoundClip;
+    [SerializeField] public Vector3 respawnPoint;
 
     public bool isDead;
 
     public PlayerMovement playerMovement;
     public HealthUI healthUI;
-    public UIManager uiManager;
 
-    // respawn?
-    public Transform respawnPosition;
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numFlashes;
+    private SpriteRenderer spriteRend;
+
+    public GameObject StompChecker;
 
     private void Start()
     {
@@ -29,6 +33,8 @@ public class PlayerHealth : MonoBehaviour
         GameData.health = maxHealth;
         healthUI.SetMaxHearts(maxHealth);
         isDead = false;
+        spriteRend = GetComponent<SpriteRenderer>();
+        StompChecker = transform.GetChild(2).gameObject;
     }
 
     private void Update()
@@ -46,6 +52,10 @@ public class PlayerHealth : MonoBehaviour
         {
             Die();
         }
+        else
+        {
+            StartCoroutine(Invulnerability());
+        }
 
     }
 
@@ -55,20 +65,19 @@ public class PlayerHealth : MonoBehaviour
         // death anim
         deathParticle.Play();
 
-        transform.position = respawnPosition.position;
-
         // All movement stops, all collision is removed and the player is destroyed afterwards.
+        // Uses the player's movement script reduce their speed and jump so they can't move.
         gameObject.GetComponent<PlayerMovement>().StopMovement();
-
-        // Rigidbody stays in place to prevent player from falling off the map mario-style.
-        rb.constraints = RigidbodyConstraints2D.FreezeAll;
-
-        // Box Collider turns into a trigger to prevent collision with enemies during death.
-        gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+        yield return new WaitForSeconds(1);
 
         // Change the number here to the duration of the death animation so the whole thing plays out.
         yield return new WaitForSeconds(3);
-        Destroy(gameObject);
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        Respawn();
+        yield return new WaitForSeconds(3);
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        isDead = false;
+        gameObject.GetComponent<PlayerMovement>().ResumeMovement();
     }
 
     public void Heal(int healAmount)
@@ -83,6 +92,27 @@ public class PlayerHealth : MonoBehaviour
     {
         Debug.Log("Health increased by 1");
         Heal(healAmount);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        // Following script causes Player to ignore enemy collision after getting hurt, and disables the jump stun temporarily.
+        Physics2D.IgnoreLayerCollision(9, 6, true);
+        StompChecker.GetComponent<BoxCollider2D>().enabled = false;
+        for (int i = 0; i < numFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(1);
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(1);
+        }
+        StompChecker.GetComponent<BoxCollider2D>().enabled = true;
+        Physics2D.IgnoreLayerCollision(9, 6, false);
+    }
+
+    public void Respawn()
+    {
+        transform.position = respawnPoint;
     }
 
 }
